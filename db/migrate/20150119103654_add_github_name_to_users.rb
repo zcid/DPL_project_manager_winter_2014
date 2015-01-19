96ed6088540b057eaa456d51ce3eca2b23132713
Carlos Garcia <carlos@devpointlabs.com>
2015-01-19 03:37:16 -0700

    Add github username to DB

---
+++ db/migrate/20150119103654_add_github_name_to_users.rb
@@ -0,0 +1,5 @@
+class AddGithubNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :github_username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119090706) do
+ActiveRecord::Schema.define(version: 20150119103654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150119090706) do
     t.string   "name"
     t.string   "github_state"
     t.string   "github_access_token"
+    t.string   "github_username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
