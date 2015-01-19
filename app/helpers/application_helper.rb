09bfe025a6bdb6047054d798d209e45476c0895a
Carlos Garcia <carlos@devpointlabs.com>
2015-01-19 02:27:26 -0700

    Added helper to generate link to Github auth page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   include ::Apis::IpInfo
+  include Apis::Github
 
   def get_weather_widget(request)
     ip = request.remote_ip unless request.remote_ip == '127.0.0.1'
@@ -9,6 +10,10 @@ module ApplicationHelper
     iframe_src = 'http://forecast.io/embed/#' + lat_and_lon.to_query
     html = '<iframe id="forecast_embed" type="text/html" frameborder="0" height="245" width="100%" src="' + iframe_src + '"> </iframe>'
     raw html
+  end
+
+  def github_authorize_button()
+    link_to "Link To Github", generate_authorize_url(current_user), class: 'btn btn-primary'
   end
 
 end
