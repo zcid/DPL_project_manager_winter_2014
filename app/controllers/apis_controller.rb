4b49af006ffb4fba82a535937a98f295e96d0566
Carlos Garcia <carlos@devpointlabs.com>
2015-01-19 03:47:58 -0700

    Save github user name

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -6,6 +6,7 @@ class ApisController < ApplicationController
     access_token = Apis::Github.get_access_token(user, params[:code])
     redirect_to root_path, notice: 'Invalid flow. Something is not right, could not get access token' unless access_token
     user.authorized_for_github!(access_token)
+    user.update_attribute(:github_username, Apis::Github.get_user_information(user))
     redirect_to user_path(user)
   end
 
